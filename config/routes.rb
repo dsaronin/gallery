cda809746fdf5553463b7a0dac4dbdf4d54ea44e
Daudi Majozi <duma.jabari@gmail.com>
2023-09-15 19:53:39 -0700

    basic - 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'portfolio/index'
+  get 'portfolio/create'
+  get 'portfolio/new'
+  get 'portfolio/show'
+  get 'portfolio/update'
+  get 'portfolio/edit'
+  get 'portfolio/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
