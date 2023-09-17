5e7a5aaf75cc35b28de1bf03910f650af1d428a3
Daudi Majozi <duma.jabari@gmail.com>
2023-09-16 20:34:28 -0700

    basic - 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  # Application config variables
+  # Rails.configuration.app_name  -- string for name of app
+  # Rails.configuration.app_versio  -- string for app version
+  # Rails.configuration.photo_path  -- string for path to photos folder
+  #
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Gallery
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+# APPLICATION constants ... #TODO replace with config file?
+    config.app_name  "Gallery"
+    config.app_version  "0.01"
+    config.photo_path  ( ENV["GALLERY_PATH"]  ||  "/home/daudi/Pictures" )
+    
+ 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
