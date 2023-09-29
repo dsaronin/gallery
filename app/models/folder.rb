7304df8d61bde32e04b05387ad0d5890170cd8bb
Daudi Majozi <duma.jabari@gmail.com>
2023-09-29 14:29:07 -0700

    folders - 7

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,3 +1,21 @@
+# ----------------------------------------------------------
+# Gallery: A Photographer's gallery/portfolio tool
+# Copyright (c) 2023 David S Anderson, All Rights Reserved
+# ----------------------------------------------------------
+
 class Folder < ApplicationRecord
+
   has_many :photos
-end
+
+  validates_presence_of  :name
+  validates_presence_of  :path
+
+# ------------------------------------------------------------------------------
+#      t.string :name
+#      t.string :path
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#
+end  # class
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,22 @@
+# ----------------------------------------------------------
+# Gallery: A Photographer's gallery/portfolio tool
+# Copyright (c) 2023 David S Anderson, All Rights Reserved
+# ----------------------------------------------------------
+
 class Photo < ApplicationRecord
+
   belongs_to :folder
   belongs_to :portfolio, optional: true
   belongs_to :talent, optional: true
+ 
+  validates :latitude, numericality: true
+  validates :longitude, numericality: true
+  validates :rating, numericality: { only_integer: true, in: (0..5), allow_nil: true }
+  validates_presence_of  :photo_dsc
+  validates_presence_of  :filename
+  validates_presence_of  :photodate
 
+# ------------------------------------------------------------------------------
 #      t.string :photo_dsc
 #      t.string :filename
 #      t.integer :rating
@@ -13,8 +27,10 @@ class Photo < ApplicationRecord
 #      t.references :folder, null: false, foreign_key: true
 #      t.references :talent, null: true, foreign_key: true
 #      t.references :portfolio, null: true, foreign_key: true
+# ------------------------------------------------------------------------------
 
 
 
 
+# ------------------------------------------------------------------------------
 end   # class  Photo
